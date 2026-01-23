341a31e73314d69e35ca2e66eac7818804716434
Jerome Wilson <jeromexy133@gmail.com>
2026-01-23 11:22:11 +0530

    adding new java class to  perfrom arithemtic operations based on opcode selected

--- src/com/java/basics/Main.java
+++ src/com/java/basics/Main.java
@@ -4,17 +4,26 @@ import java.util.Scanner;
 public class Main{
     public static void main( String[] args) {
         Scanner sc = new Scanner(System.in);
-        NumberCheck numberCheck = new NumberCheck();
-        System.out.println("Enter any number: ");
-        int input = sc.nextInt();
-        System.out.println(numberCheck.numberSignCheck(input));
+//        NumberCheck numberCheck = new NumberCheck();
+//        System.out.println("Enter any number: ");
+//        int input = sc.nextInt();
+//        System.out.println(numberCheck.numberSignCheck(input));
+//
+//        CharacterU charU = new CharacterU();
+//        PrefixPostfix prefixpostfix = new PrefixPostfix();
 
-        CharacterU charU = new CharacterU();
-        PrefixPostfix prefixpostfix = new PrefixPostfix();
-
-        //charU.printCharacters();
-        //prefixpostfix.demonstratePrefixPostfix();
-
+//        charU.printCharacters();
+//        prefixpostfix.demonstratePrefixPostfix();
+        System.out.print("Enter the first number: ");
+        double firstNumber = sc.nextDouble();
+        System.out.print("Enter the second number: ");
+        double secondNumber = sc.nextDouble();
+        System.out.println("Enter the opcode for doing the arithemetic operations:");
+        System.out.println("NOTE: The OpCode for addition is 'a', for subtraction it is 's', for multiplication it is 'm' and for division it is 'd'");
+        System.out.print("Enter the value: ");
+        char opCode = sc.next().charAt(0);
+        SwitchOpCode opCodeCalculator = new SwitchOpCode();
+        opCodeCalculator.opCodeOperations(opCode, firstNumber, secondNumber);
         sc.close();
     }
 }
---
+++ src/com/java/basics/SwitchOpCode.java
@@ -0,0 +1,36 @@
+package com.java.basics;
+
+public class SwitchOpCode {
+    public void opCodeOperations(char opCode, double firstnumber, double secondnumber) {
+        double result;
+        switch (opCode) {
+            case 'a':
+                result = firstnumber + secondnumber;
+                System.out.println("performing addition...");
+                System.out.println(result);
+                break;
+
+            case 's':
+                result = firstnumber - secondnumber;
+                System.out.println("performing subtraction...");
+                System.out.println(result);
+                break;
+            case 'm':
+                result = firstnumber * secondnumber;
+                System.out.println("performing multiplication...");
+                System.out.println(result);
+                break;
+            case 'd':
+                if (secondnumber != 0.0) {
+                    result = firstnumber / secondnumber;
+                    System.out.println("performing division...");
+                    System.out.println(result);
+                    break;
+                } else {
+                    throw new ArithmeticException("Division by zero is not allowed!");
+                }
+            default:
+                throw new IllegalArgumentException("Invalid OpCode: " + opCode);
+        }
+    }
+}
