994031eb7d3c6c2a467141bd5149fda02f3f9e19
Jerome Wilson <jeromexy133@gmail.com>
2026-02-01 20:21:40 +0530

    1342. Number of Steps to Reduce a Number to Zero

---
+++ src/com/java/basics/leetcode/numberofsteps/NumberOfStepsToZero.java
@@ -0,0 +1,17 @@
+package com.java.basics.leetcode.numberofsteps;
+
+public class NumberOfStepsToZero {
+
+    public int numberOfSteps(int num) {
+        int steps = 0;
+        while (num > 0) {
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num -= 1;
+            }
+            steps++;
+        }
+        return steps;
+    }
+}
