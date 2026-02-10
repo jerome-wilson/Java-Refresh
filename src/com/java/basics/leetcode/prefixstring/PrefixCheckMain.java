bb0661fc8ed9dc83484c50e8da913299255c7dd6
Jerome Wilson <jeromexy133@gmail.com>
2026-02-10 10:55:23 +0530

    Modified the code to tackle a bug and a better practice

--- src/com/java/basics/leetcode/prefixstring/PrefixCheckMain.java
+++ src/com/java/basics/leetcode/prefixstring/PrefixCheckMain.java
@@ -5,12 +5,9 @@ public class PrefixCheckMain {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
         sc.nextLine();
-
         String[] strs = new String[n];
-
         for (int i = 0; i < n; i++) {
             strs[i] = sc.nextLine();
         }
