25e4fcfc2a27ca02a04cdd75a9bf13e99a33cdf6
Jerome Wilson <jeromexy133@gmail.com>
2026-02-16 11:52:57 +0530

    20. Valid Paranthesis

--- src/com/java/basics/leetcode/paranthesischeck/ValidParantheses.java
+++ src/com/java/basics/leetcode/paranthesischeck/ValidParantheses.java
@@ -1,5 +1,23 @@
 package com.java.basics.leetcode.paranthesischeck;
 
+import java.util.Stack;
+
 public class ValidParantheses {
-    
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch: s.toCharArray()) {
+            if(ch == '{' || ch == '[' || ch == '(') {
+                stack.add(ch);
+            } else {
+                if(stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if((ch == '}' && top != '{') ||(ch == ']' && top != '[') || (ch == ')' && top != '(') ) {
+                    return false;
+                }
+            }
+        }
+            return stack.isEmpty();
+    }
 }
