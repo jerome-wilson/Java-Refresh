bd1f174c948fd16c743247c5b79446a532cecf4b
Jerome Wilson <jeromexy133@gmail.com>
2026-03-06 13:38:29 +0530

    Added concepts of Abstraction

---
+++ src/com/java/basics/bits/oops/Shape.java
@@ -0,0 +1,13 @@
+package com.java.basics.bits.oops;
+
+public class Shape {
+    public static void main(String[] args) {
+        Circle circle = new Circle(10);
+        Triangle triangle = new Triangle(6,2);
+        Rectangle rectangle = new Rectangle(20, 13);
+        circle.display();
+        System.out.println(circle.area());
+        System.out.println(triangle.area());
+        System.out.println(rectangle.area());
+    }
+}
