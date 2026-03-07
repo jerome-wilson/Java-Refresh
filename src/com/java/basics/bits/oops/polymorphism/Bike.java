f385e55ad2b405c86dbbf8ae3841ace2cdb39ca1
Jerome Wilson <jeromexy133@gmail.com>
2026-03-07 14:34:02 +0530

    Added program explaining Interface and polymorphism in Java

---
+++ src/com/java/basics/bits/oops/polymorphism/Bike.java
@@ -0,0 +1,7 @@
+package com.java.basics.bits.oops.polymorphism;
+
+public class Bike implements Interface{
+    public void go() {
+        System.out.println("You are driving a bike...");
+    }
+}
