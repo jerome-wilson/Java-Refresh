dd818646f4497fbd4bdbe23cd4ad1f6b65fa1a39
Jerome Wilson <jeromexy133@gmail.com>
2026-03-08 23:29:30 +0530

    added a program with array operations in dsa class

---
+++ src/com/java/basics/bits/oops/arrays/MatrixAdd.java
@@ -0,0 +1,41 @@
+package com.java.basics.bits.oops.arrays;
+
+public class MatrixAdd {
+    public static void main(String[] args) {
+        int[][] arr = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        MatrixAdd adding = new MatrixAdd();
+        System.out.println(adding.add(arr));
+        int[] rowSums = adding.rowSum(arr);
+        for(int i = 0; i < rowSums.length; i++) {
+            System.out.println("Row "+(i+1)+" sum "+rowSums[i]);
+        }
+    
+    }
+
+    int add(int[][] array){
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                sum += array[i][j];
+            }
+        }
+        return sum;
+    }
+
+    int[] rowSum(int[][] array) {
+        int[] sums = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            int rowValue = 0;
+            for (int j = 0; j < array[i].length; j++) {
+                rowValue += array[i][j];
+            }
+            sums[i] = rowValue;
+        }
+        return sums;
+    }
+}
+
