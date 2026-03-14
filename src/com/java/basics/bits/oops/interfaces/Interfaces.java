3077c01b9f347c7600eff3e6323fe8e07245c4b5
Jerome Wilson <jeromexy133@gmail.com>
2026-03-14 23:03:25 +0530

    declared inner interface

--- src/com/java/basics/bits/oops/interfaces/Interfaces.java
+++ src/com/java/basics/bits/oops/interfaces/Interfaces.java
@@ -1,5 +1,7 @@
 package com.java.basics.bits.oops.interfaces;
 
 public interface Interfaces {
-
+    public interface InnerInterface {
+        // inner interface methods can be declared here
+    }
 }
