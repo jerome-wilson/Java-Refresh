ea2ae79b971f34950c02c7de43f1afe7d76ff415
Jerome Wilson <jeromexy133@gmail.com>
2026-04-04 08:55:06 +0530

    Day 4: Add ReverseLinkedListLogic class to implement linked list reversal

---
+++ src/com/java/basics/dailypractice/day4/ReverseLinkedListLogic.java
@@ -0,0 +1,30 @@
+package com.java.basics.dailypractice.day4;
+
+public class ReverseLinkedListLogic {
+
+    // Definition for singly-linked list
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public static ListNode reverseList(ListNode head) {
+
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode nextTemp = current.next; // store next
+            current.next = prev;              // reverse link
+            prev = current;                   // move prev forward
+            current = nextTemp;               // move current forward
+        }
+
+        return prev;
+    }
+}
