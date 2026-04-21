345f26e6efdfdd5c9fbc13526012b3f05cfe42e3
Jerome Wilson <jeromexy133@gmail.com>
2026-04-21 23:11:00 +0530

    Add ValidPalindromeLogic and ValidPalindromeMain classes for palindrome check

---
+++ src/com/java/basics/dailypractice/day9/ValidPalindromeMain.java
@@ -0,0 +1,14 @@
+package com.java.basics.dailypractice.day9;
+public class ValidPalindromeMain {
+
+    public static void main(String[] args) {
+
+        ValidPalindromeLogic obj = new ValidPalindromeLogic();
+
+        String input = "A man, a plan, a canal: Panama";
+
+        boolean result = obj.isPalindrome(input);
+
+        System.out.println("Is Palindrome: " + result);
+    }
+}
