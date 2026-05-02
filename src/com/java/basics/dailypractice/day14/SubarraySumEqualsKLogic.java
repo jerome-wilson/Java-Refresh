98d7075a8e0d570790f3ff3e46050901bf933b38
Jerome Wilson <jeromexy133@gmail.com>
2026-05-02 23:27:29 +0530

    day14: added logic

---
+++ src/com/java/basics/dailypractice/day14/SubarraySumEqualsKLogic.java
@@ -0,0 +1,28 @@
+package com.java.basics.dailypractice.day14;
+
+import java.util.HashMap;
+
+public class SubarraySumEqualsKLogic {
+
+    public int subarraySum(int[] nums, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1); // important base case
+
+        int currentSum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+
+            currentSum += num;
+
+            if (map.containsKey(currentSum - k)) {
+                count += map.get(currentSum - k);
+            }
+
+            map.put(currentSum, map.getOrDefault(currentSum, 0) + 1);
+        }
+
+        return count;
+    }
+}
