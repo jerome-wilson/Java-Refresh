7364330772cd226070d64a09f60fe70951d97790
Jerome Wilson <jeromexy133@gmail.com>
2026-05-24 23:07:24 +0530

    Added logic

---
+++ src/com/java/basics/dailypractice/day20/MaximumDepthBinaryTreeLogic.java
@@ -0,0 +1,27 @@
+package com.java.basics.dailypractice.day20;
+
+class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class MaximumDepthBinaryTreeLogic {
+
+    public int maxDepth(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+
+        return 1 + Math.max(leftDepth, rightDepth);
+    }
+}
