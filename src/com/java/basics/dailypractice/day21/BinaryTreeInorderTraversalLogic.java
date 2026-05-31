751d92a978a0d046f6901f31fb489bc6abcff6b4
Jerome Wilson <jeromexy133@gmail.com>
2026-05-31 23:06:39 +0530

    Day 21: Added BinaryTreeInorderTraversalLogic logic file

---
+++ src/com/java/basics/dailypractice/day21/BinaryTreeInorderTraversalLogic.java
@@ -0,0 +1,40 @@
+package com.java.basics.dailypractice.day21;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class BinaryTreeInorderTraversalLogic {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        List<Integer> result = new ArrayList<>();
+
+        inorder(root, result);
+
+        return result;
+    }
+
+    private void inorder(TreeNode node, List<Integer> result) {
+
+        if (node == null) {
+            return;
+        }
+
+        inorder(node.left, result);
+
+        result.add(node.val);
+
+        inorder(node.right, result);
+    }
+}
