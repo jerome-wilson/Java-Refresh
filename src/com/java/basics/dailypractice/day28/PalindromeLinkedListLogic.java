7bc2c2eab1b1b87829767d150f18623c0de006cb
Jerome Wilson <jeromexy133@gmail.com>
2026-06-16 08:43:25 +0530

    Add PalindromeLinkedListLogic.java for Day 28 - Palindrome Linked List

---
+++ src/com/java/basics/dailypractice/day28/PalindromeLinkedListLogic.java
@@ -0,0 +1,58 @@
+package com.java.basics.dailypractice.day28;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class PalindromeLinkedListLogic {
+
+    public boolean isPalindrome(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        // Find middle
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Reverse second half
+        ListNode secondHalf = reverseList(slow.next);
+        ListNode firstHalf = head;
+
+        // Compare
+        while (secondHalf != null) {
+            if (firstHalf.val != secondHalf.val) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+}
