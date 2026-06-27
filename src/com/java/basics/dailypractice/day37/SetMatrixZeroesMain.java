0325584703d663d093f45fb8b37b2779483326e9
Jerome Wilson <jeromexy133@gmail.com>
2026-06-27 19:55:14 +0530

    Add SetMatrixZeroesMain.java for Day 37 - Test cases

---
+++ src/com/java/basics/dailypractice/day37/SetMatrixZeroesMain.java
@@ -0,0 +1,31 @@
+package com.java.basics.dailypractice.day37;
+
+import java.util.Arrays;
+
+public class SetMatrixZeroesMain {
+
+    public static void main(String[] args) {
+
+        SetMatrixZeroesLogic obj = new SetMatrixZeroesLogic();
+
+        // Test case
+        int[][] matrix = {
+            {1, 1, 1},
+            {1, 0, 1},
+            {1, 1, 1}
+        };
+
+        System.out.println("Before:");
+        for (int[] row : matrix) {
+            System.out.println(Arrays.toString(row));
+        }
+
+        obj.setZeroes(matrix);
+
+        System.out.println("\nAfter:");
+        for (int[] row : matrix) {
+            System.out.println(Arrays.toString(row));
+        }
+        // Expected: [[1,0,1],[0,0,0],[1,0,1]]
+    }
+}
