a6d18471db89b1c28b8f6d957a41e1a9c439a4b5
Jerome Wilson <jeromexy133@gmail.com>
2026-07-11 23:32:15 +0530

    Add TopKFrequentElementsLogic.java for Day 45 - Top K Frequent Elements

---
+++ src/com/java/basics/dailypractice/day45/TopKFrequentElementsLogic.java
@@ -0,0 +1,43 @@
+package com.java.basics.dailypractice.day45;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TopKFrequentElementsLogic {
+
+    public int[] topKFrequent(int[] nums, int k) {
+
+        // Count frequencies
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        // Bucket sort - index is frequency
+        List<Integer>[] buckets = new List[nums.length + 1];
+        for (int key : freqMap.keySet()) {
+            int freq = freqMap.get(key);
+            if (buckets[freq] == null) {
+                buckets[freq] = new ArrayList<>();
+            }
+            buckets[freq].add(key);
+        }
+
+        // Collect top k elements
+        int[] result = new int[k];
+        int index = 0;
+
+        for (int i = buckets.length - 1; i >= 0 && index < k; i--) {
+            if (buckets[i] != null) {
+                for (int num : buckets[i]) {
+                    result[index++] = num;
+                    if (index == k) break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
