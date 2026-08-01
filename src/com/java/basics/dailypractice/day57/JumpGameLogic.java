fac11724c1a4f1dbe375947f0e4aa4fdad930956
Jerome Wilson <jeromexy133@gmail.com>
2026-08-01 23:15:43 +0530

    Add JumpGameLogic.java for Day 57 - Jump Game

---
+++ src/com/java/basics/dailypractice/day57/JumpGameLogic.java
@@ -0,0 +1,24 @@
+package com.java.basics.dailypractice.day57;
+
+public class JumpGameLogic {
+
+    public boolean canJump(int[] nums) {
+
+        int maxReach = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (i > maxReach) {
+                return false;
+            }
+
+            maxReach = Math.max(maxReach, i + nums[i]);
+
+            if (maxReach >= nums.length - 1) {
+                return true;
+            }
+        }
+
+        return true;
+    }
+}
