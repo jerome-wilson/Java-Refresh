50e987504368a4380fa3b0c77680d7f727f044ca
Jerome Wilson <jeromexy133@gmail.com>
2026-08-14 18:19:33 +0530

    Add GroupAnagramsLogic.java for Day 61 - Group Anagrams

---
+++ src/com/java/basics/dailypractice/day61/GroupAnagramsLogic.java
@@ -0,0 +1,28 @@
+package com.java.basics.dailypractice.day61;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GroupAnagramsLogic {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String sorted = new String(chars);
+
+            if (!map.containsKey(sorted)) {
+                map.put(sorted, new ArrayList<>());
+            }
+            map.get(sorted).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
