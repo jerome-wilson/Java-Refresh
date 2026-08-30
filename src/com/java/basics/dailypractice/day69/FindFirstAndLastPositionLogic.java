58d64d73be3ae96fdef6e62231a659afe272e785
Jerome Wilson <jeromexy133@gmail.com>
2026-08-30 22:40:49 +0530

    Add FindFirstAndLastPositionLogic.java for Day 69 - Find First and Last Position

---
+++ src/com/java/basics/dailypractice/day69/FindFirstAndLastPositionLogic.java
@@ -0,0 +1,60 @@
+package com.java.basics.dailypractice.day69;
+
+public class FindFirstAndLastPositionLogic {
+
+    public int[] searchRange(int[] nums, int target) {
+
+        int[] result = {-1, -1};
+
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+
+        result[0] = findFirst(nums, target);
+        result[1] = findLast(nums, target);
+
+        return result;
+    }
+
+    private int findFirst(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                result = mid;
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+
+    private int findLast(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                result = mid;
+                left = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+}
