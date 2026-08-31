4f62973ebfd8519cdd7988ab982aa7598d3ffdac
Jerome Wilson <jeromexy133@gmail.com>
2026-08-31 13:37:55 +0530

    Add SearchA2DMatrixLogic.java for Day 70 - Search a 2D Matrix

---
+++ src/com/java/basics/dailypractice/day70/SearchA2DMatrixLogic.java
@@ -0,0 +1,32 @@
+package com.java.basics.dailypractice.day70;
+
+public class SearchA2DMatrixLogic {
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int left = 0;
+        int right = m * n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midValue = matrix[mid / n][mid % n];
+
+            if (midValue == target) {
+                return true;
+            } else if (midValue < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
